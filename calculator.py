afabe188f3fbbfb76def328b0cb7d069657c6eb3
n01147386 <n01147386@humbermail.ca>
2019-03-31 02:57:35 -0400

    calc menu loop

--- calculator.py
+++ calculator.py
@@ -1,14 +1,6 @@
-print("Choose the function you want to use")
-print("1 for Addition")
-print("2 for Subtraction")
-print("3 for Multiplication")
-print("4 for Division")
-
-# input 
-inputMode = str(input()) 
-
 argOne = 0
 argTwo = 0
+inputMode = ""
 
 def promptForNumbers():
     print("Enter the first number")
@@ -34,21 +26,40 @@ def division():
     val = argOne / argTwo
     return str(val)
 
-def presentOptions():
-    #print("You chose : " + inString)
-    if (inputMode == "1"):
+
+def processRequest():
+#print("You chose : " + inString)
+    if (inputMode == "0"):
+        quit()
+    elif (inputMode == "1"):
         promptForNumbers()
         print("The answer is : " + addition())
+        mainMenu()
     elif (inputMode == "2"):
         promptForNumbers()
         print("The answer is : " + subtraction())
+        mainMenu()
     elif (inputMode == "3"):
         promptForNumbers()
         print("The answer is : " + multiplication())
+        mainMenu()
     elif (inputMode == "4"):
         promptForNumbers()
         print("The answer is : " + division())
+        mainMenu()
     else : print ("You chose invalid option")
 
+def mainMenu():
+    print("Choose the function you want to use")
+    print("0 to quit")
+    print("1 for Addition")
+    print("2 for Subtraction")
+    print("3 for Multiplication")
+    print("4 for Division")
+    global inputMode
+    inputMode = str(input()) 
+    processRequest()
 
-presentOptions()
+mainMenu()
+
+
